c6b3e519a4850e60eacd6e993f379564da3cc348
saad130013 <saad130013@gmail.com>
2025-12-02 01:10:05 +0300

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBchN2Wd5vRzSHDQ0BN7d9Am6s43wS1xCY",
-  authDomain: "inspectionssys-a764d.firebaseapp.com",
-  projectId: "inspectionssys-a764d",
-  storageBucket: "inspectionssys-a764d.appspot.com",
-  messagingSenderId: "823282027878",
-  appId: "1:823282027878:web:aa55ef272af33821df88ca7",
-  measurementId: "G-099GCJHKBD"
+  apiKey: "AIzaSyD0OZWWkKrB4O3wVfCZgb5a7QELkOsdilw",
+  authDomain: "layla-966.firebaseapp.com",
+  projectId: "layla-966",
+  storageBucket: "layla-966.firebasestorage.app",
+  messagingSenderId: "646607727400",
+  appId: "1:646607727400:web:14d624f85792d274293e1d",
+  measurementId: "G-WRTHMQR3H8"
 };
 
 const app = initializeApp(firebaseConfig);
